86388559f4e211701b4001557348cfa9569486ae
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2022-08-07 22:54:31 -0300

    [FEAT] - Section 1 Requeriments - some tests

--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -2,19 +2,21 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import { before } from 'mocha';
 
 import { app } from '../app';
 import Users from '../database/models/Users';
-
-import { Response } from 'superagent';
+import login from '../services/login.service';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Users Entity', () => {
+describe('Testes da Model de User', () => {
   it('Model Users deve ter uma propriedade id', () => {
 		const newUser = new Users();
+
+		newUser.id = 1;
 
 		expect(newUser.id).to.be.equal(1);
 	});
@@ -42,8 +44,63 @@ describe('Users Entity', () => {
 
 		expect(newUser.email).to.be.equal('admin@gmail.com');
 	});
+});
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+const user = [{
+	username: 'User',
+	role: 'user',
+	email: 'user@user.com',
+	password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
+}];
+
+const correctLoginInfo = {
+	email: 'user@user.com',
+	password: 'secret_user',
+};
+
+const incorrectLoginInfo = {
+	email: '',
+	password: '',
+};
+
+describe('Testes da rota de login', () => {
+	before(() => {
+		sinon.stub(Users, 'findAll')
+		  .resolves([user[0]] as Users[]);
+	});
+
+	after(() => {(
+		Users.findAll as sinon.SinonStub
+	).restore()})
+
+	
+	it('O login é feito com sucesso', async () => {
+		const newLogin = await chai.request(app).post('/login').send(correctLoginInfo);
+		expect(newLogin.status).to.be.equal(200);
+		expect(newLogin.body).to.be.a('object');
+	});
+	
+	it('O login não é efetuado com sucesso', async () => {
+		const newLogin = await chai.request(app).post('/login').send(incorrectLoginInfo);
+		expect(newLogin.status).to.be.equal(400);
+		expect(newLogin.body).to.be.a('object');
+	});
+});
+
+describe('Testes do service de login', () => {
+	before(() => {
+		sinon.stub(Users, 'findAll')
+			.resolves([user[0]] as Users[]);
+	});
+
+	after(() => {
+		(
+			Users.findAll as sinon.SinonStub
+		).restore()
+	})
+
+	it('A função "login" retorna o usuário com sucesso', async () => {
+		const getUser = await login(correctLoginInfo);
+		expect(getUser).to.be.a('object');
+	});
 });
